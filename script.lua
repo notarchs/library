f7c913d63e2f929f02d3c4dcb5c3f42a57226af6
notarchs <90525306+notarchs@users.noreply.github.com>
2022-06-03 15:49:45 +0100

    Update script.lua

--- script.lua
+++ script.lua
@@ -471,6 +471,19 @@ wait(.5)
 game:GetService('TeleportService'):Teleport(game.PlaceId)
 end)
 
+sec3:Button("Rejoin Server", function()
+
+    ArchsUI:Notification("Rejoin Server", "Rejoining Server...") wait(1)
+    game:GetService("Players").LocalPlayer.OnTeleport:Connect(function(State)
+        repeat wait() until game:IsLoaded() 
+            syn.queue_on_teleport('loadstring(game:HttpGet("https://raw.githubusercontent.com/notarchs/library/main/script.lua"))()')
+        end); 
+    game:GetService('TeleportService'):Teleport(game.PlaceId, Players.LocalPlayer)
+else
+    game:GetService('TeleportService'):TeleportToPlaceInstance(game.PlaceId, game.JobId, Players.LocalPlayer)
+
+end)
+
 sec3:Textbox("Power Output", false, function(poweroutput)
   print(poweroutput)
   shared.powerOutput = poweroutput
